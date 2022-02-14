58e4653c64a7171772b781a7a070383462db170e
Jordan Lin <68578258+kuanhenglin@users.noreply.github.com>
2022-02-13 16:47:05 -0800

    Update SignIn.js

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -34,7 +34,7 @@ function SignIn(props) {
 
   async function handleSignIn() {
     const user = await getUser(username);
-    
+ 
     if (user && user.password == password) {  // check that password matches
       props.setCookie("username", username, { path: "/" });
       routeChange("/profile");
