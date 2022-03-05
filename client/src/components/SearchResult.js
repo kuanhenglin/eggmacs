e114cc5952a3e37e0eff28c72f56853e5028cce3
the-bay-kay <katie.risc@gmail.com>
2022-03-05 14:10:37 -0800

    Fixed blank search & added map search
    Fixed bug where empty search wouldn't return all objects within
    a collection, and updated the search webpage to include map searching!
    Map searching acts identically to userSearch, using the same generalized
    algorithm.

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -4,7 +4,22 @@ import { useCookies } from 'react-cookie';
 function SearchResultMap(props) {
   return (
     <div>
-
+      {props.maps.map(map => {
+        return(
+          <p className="search-result" key={map._id}>
+            <b>
+              <Link
+                className="hypertext"
+                to={`/map/${map._id}`}
+              >
+                {map.displayName}  
+              </Link>
+            </b> 
+            &nbsp;(<span className="mapname">{map._id}</span>)<br />
+            <i>{map.description}</i><br />
+          </p>
+        )
+      })}
     </div>
   );
 }
