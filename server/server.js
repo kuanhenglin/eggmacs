30c9070d598b1237a170e387254fb75c5b0c73ce
Sean Cunin <sccunin@gmail.com>
2022-03-06 15:06:05 -0800

    hosting prep/api generalizations
    8 hours of blood, sweat, and tears went into these 20 changed lines

--- server/server.js
+++ server/server.js
@@ -10,6 +10,17 @@ app.use(require("./routes/searchRoutes"));
 // get driver connection
 const dbo = require("./db/conn");
  
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+ 
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (error) {
