52e4f5d8b701ce3566bf07606dfed3674bf81979
Sean Cunin <sccunin@gmail.com>
2022-03-06 16:38:59 -0800

    changed location of index.html file?
    idk if itll work lol

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname + '/app/client/public/index.html'));
   });
 }
  
